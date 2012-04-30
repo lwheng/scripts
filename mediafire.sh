c070b3edb9b433c4c0ecb62208d538ad2c97dd29
Hong Dai Thanh <nhahtdh@gmail.com>
2012-04-30 18:50:56 +0800

    Add support for system without wget

--- mediafire.sh
+++ mediafire.sh
@@ -15,6 +15,34 @@
 # Thanks to rctay for regex
 # Thanks to benjamin for idea of curl POST-ing
 
+# Check whether curl is installed
+command -v curl >/dev/null 2>&1 || {
+  echo "$0: curl utility not found." >&2
+  echo "$0: Please install curl package or check PATH variable." >&2
+  exit 1
+}
+
+# Check whether wget is installed
+WGET_INSTALLED=true
+
+command -v wget >/dev/null 2>&1 || { 
+  WGET_INSTALLED=false
+}
+
+function help() {
+   echo -e \
+        "Usage:\n" \
+        "  $0 INPUT_FILE\n\n" \
+        "    INPUT_FILE\n" \
+        "      A text file containing links to files hosted on Mediafire\n" \
+        "      Each link should be on one line\n" >&2;
+}
+
+if [[ -z "$1" ]]; then
+  help
+  exit 1;
+fi
+
 INPUTFILE=$1
 
 for LINK in $(cat $INPUTFILE)
@@ -24,12 +52,21 @@ for LINK in $(cat $INPUTFILE)
     RESULT=$(curl $temp | tr "\"" "\n" | grep -E 'http://([0-9]{1,3}\.){3}([0-9]{1,3})')
     if [ -z "$RESULT" ]
     then
-    echo "$temp is password protected"
-    echo "Please enter password:"
-    read password
-    RESULT1=$(curl -d "downloadp=$password" $temp | tr "\"" "\n" | grep -E 'http://([0-9]{1,3}\.){3}([0-9]{1,3})')
-    wget -b --quiet $RESULT1
+      echo "$temp is password protected"
+      echo "Please enter password:"
+      read password
+      RESULT1=$(curl -d "downloadp=$password" $temp | tr "\"" "\n" | grep -E 'http://([0-9]{1,3}\.){3}([0-9]{1,3})')
+      if $WGET_INSTALLED; then
+        wget -b --quiet $RESULT1
+      else
+        curl -L -O -J $RESULT1 &
+      fi
     else
-    wget -b --quiet $RESULT
+      if $WGET_INSTALLED; then
+        echo "Down"
+        wget -b --quiet $RESULT
+      else
+        curl -L -O -J $RESULT &
+      fi
     fi
-    done
+done
